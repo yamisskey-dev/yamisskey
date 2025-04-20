668763493c48fa7047915bfeeeaf1456aab36062
hitalin <hitalin@proton.me>
2025-04-20 14:36:27 +0900

    feat: added ability to filter yamiTimeline based on follow status
    now allows independent control of yamiNotes display for following/non-following users.
    
    - Added yamiNonFollowingPublicNotes stream to separate distribution based on follow status
    - Separate notes for following users from public notes for non-following users
    - Properly track changes in follow status in back-end stream channel
    - Front Maintain filtering configuration UI on the end
    
    to reflect proper filtering when follow status changes.

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -11892,6 +11892,14 @@ export interface Locale extends ILocale {
          * やみタイムライン
          */
         "yamiTimeline": string;
+        /**
+         * フォローしていないユーザーのパブリックやみノートを表示
+         */
+        "showYamiNonFollowingPublicNotes": string;
+        /**
+         * フォローしているユーザーのやみノートを表示
+         */
+        "showYamiFollowingNotes": string;
         "_yamiModeSwitcher": {
             /**
              * やみモードに切り替えますか？やみモードにすると、やみノートを閲覧・投稿できるようになります
