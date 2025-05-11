b98cb0b53c19e291e2c4ca2ec9a5b8ba717761fd
hitalin <hitalin@proton.me>
2025-05-11 10:30:26 +0900

    fix: Require login authorization to view emoji list and search

--- packages/frontend/src/router.definition.ts
+++ packages/frontend/src/router.definition.ts
@@ -253,6 +253,7 @@ export const ROUTE_DEF = [{
 }, {
 	path: '/search',
 	component: page(() => import('@/pages/search.vue')),
+	loginRequired: true,
 	query: {
 		q: 'query',
 		userId: 'userId',
@@ -370,6 +371,7 @@ export const ROUTE_DEF = [{
 }, {
 	path: '/custom-emojis-manager',
 	component: page(() => import('@/pages/custom-emojis-manager.vue')),
+	loginRequired: true,
 }, {
 	path: '/avatar-decorations',
 	name: 'avatarDecorations',
